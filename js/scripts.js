7a43c445a71127857ebdc9c55f104c9db2f1b384
MohammedBasha <me@mohammedbasha.com>
2022-05-14 00:22:16 +0200

    Using the Clipboard API instead of the execComman deprecated method

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,16 @@ const popup = document.querySelector('.content .popup'); // Grabbing the .popup 
 // Add click event on the H2 Element
 text.addEventListener('click', () => {
     popup.classList.add('active'); // add .active class to the .popup div
-    copyToClipboard(); // call the copy function
+    copyToClipboard(text); // call the copy function
 });
 
 // add animationend event on the .popup div
 popup.addEventListener('animationend', () => {
     popup.classList.remove('active');
-})
+});
 
 // the Copy to Clipboard function
-function copyToClipboard() {
-    const textArea = document.createElement('textarea'); // Create textarea element
-    textArea.setAttribute('readonly', ''); // make it readonly
-    
-    // add some styles to the textarea
-    textArea.style.cssText = `
-        position: absolute;
-        opacity: 0;
-        pointer-events: none;
-    `;
-    textArea.value = text.innerText; // add the H2 text to the textarea element
-    document.body.appendChild(textArea); // append the text area to the body
-    textArea.select(); // select the textarea's text
-    document.execCommand('copy'); // copy the text to the clipboard
-    document.body.removeChild(textArea); // remove the textarea from the body
+function copyToClipboard(elm) {
+    navigator.clipboard.writeText(elm.innerText); // Using the writeText method to write the text in the clipboard
+    navigator.clipboard.readText();// using the readText method to read the copied text
 }
