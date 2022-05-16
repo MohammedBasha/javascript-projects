a54352b5d4335f6231db7b585e6761b6597a0047
MohammedBasha <me@mohammedbasha.com>
2022-05-16 21:07:25 +0200

    Fetching the jokes with Promise chains

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('.container button'); // the Generator Button
 const jokeText = document.querySelector('.container p'); // the Joke text
 
+/*
 // Declaring the fetching a joke function
 async function getJoke() { // using async to asynchronize the fetching
     const jokeData = await fetch('https://icanhazdadjoke.com/', { // the joke api
@@ -11,6 +12,17 @@ async function getJoke() { // using async to asynchronize the fetching
 
     const jokeObj = await jokeData.json(); // format the output in json
     jokeText.innerHTML = jokeObj.joke; // add it to the joke text
+}
+*/
+
+function getJoke() { // using Promise chains to fetch the jokes
+    const jokeData = fetch('https://icanhazdadjoke.com/', { // the joke api
+        headers: {
+            'Accept': 'application/json' // accepting json format
+        }
+    })
+        .then(jokeObj => jokeObj.json()) // format the output in json
+        .then(formatedData => jokeText.innerHTML = formatedData.joke); // add it to the joke text
 }
 
 // when the document loaded, add a joke in the joke text
