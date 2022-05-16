7d7364e4f772f49ec3ab3e4b66d640d18acc697c
MohammedBasha <me@mohammedbasha.com>
2022-05-16 20:58:33 +0200

    Adding the joke functionality with async await fetching

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('.container button'); // the Generator Button
+const jokeText = document.querySelector('.container p'); // the Joke text
+
+// Declaring the fetching a joke function
+async function getJoke() { // using async to asynchronize the fetching
+    const jokeData = await fetch('https://icanhazdadjoke.com/', { // the joke api
+        headers: {
+            'Accept': 'application/json' // accepting json format
+        }
+    });
+
+    const jokeObj = await jokeData.json(); // format the output in json
+    jokeText.innerHTML = jokeObj.joke; // add it to the joke text
+}
+
+// when the document loaded, add a joke in the joke text
+document.addEventListener('DOMContentLoaded', getJoke);
+
+// when clicking the generator button, generate a new joke
+button.addEventListener('click', getJoke);
