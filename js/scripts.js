8eb38f47aee2e91771a87548aa9bbe174c7ef559
MohammedBasha <me@mohammedbasha.com>
2022-05-19 12:52:50 +0200

    Adding the switcher functionality

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const switcher = document.querySelector('#switcher');
+
+switcher.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        document.body.setAttribute('data-theme', 'dark');
+    } else {
+        document.body.setAttribute('data-theme', 'light');
+    }
+});
